34ec53e72a3659b3bdd51f4e3131bc3b881202df
June Tate-Gans <jtgans@google.com>
2018-07-24 14:32:10 -0700

    apex: Add in a PCI-based power controller driver
    This driver gives us more direct control over the PCI hotplug state of
    the Apex device. Effectively this allows us to keep the device loaded
    when a user space program has a file descriptor open to the device, and
    automatically removes the root PCI bus from the PCI tree when the file
    descriptor is cleaned up.
    
    Change-Id: I251afbfffa6c5d28755a2845db35a4bf76c71f72

---
+++ drivers/char/apex-power.c
@@ -0,0 +1,156 @@
+/*
+ * Driver to expose PCI-express hot-removal of devices via file
+ * handles.
+ *
+ * Copyright (C) 2018 Google, Inc.
+ * Author: June Tate-Gans <jtgans@google.com>
+ *
+ * Shamelessly the general structure of the character driver has been
+ * borrowed from bsr.c, the POWER architecture's method for accessing
+ * BSR registers.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/types.h>
+#include <linux/module.h>
+
+#include <asm/cmpxchg.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/of.h>
+#include <linux/of_platform.h>
+#include <linux/pci.h>
+
+#define APEX_PCI_VENDOR_ID 0x1ac1
+#define APEX_PCI_DEVICE_ID 0x089a
+
+static struct class *apex_power_class;
+static struct device *apex_power_device;
+static struct cdev apex_power_cdev;
+static int apex_power_major;
+static bool apex_power_owned;
+
+static int apex_power_release(struct inode* inode, struct file* filep)
+{
+	struct pci_dev *apex_dev = NULL;
+	struct pci_dev *apex_connected_bus = NULL;
+
+	if (apex_power_owned != true) {
+		return -EPERM;
+	}
+
+	apex_dev = pci_get_device(APEX_PCI_VENDOR_ID, APEX_PCI_DEVICE_ID, NULL);
+	if (!apex_dev) {
+		printk(KERN_ERR "apex_power: can't find Apex on PCI bus?!\n");
+		return -EIO;
+	}
+	apex_connected_bus = apex_dev->bus->self;
+	pci_dev_put(apex_dev);
+
+	pci_stop_and_remove_bus_device_locked(apex_connected_bus);
+
+	apex_power_owned = false;
+	return 0;
+}
+
+static int apex_power_open(struct inode* inode, struct file* filep)
+{
+	struct pci_bus *pci_bus = NULL;
+	struct pci_dev *apex_dev = NULL;
+	int result = 0;
+
+	if (apex_power_owned != false) {
+		return -EPERM;
+	}
+	apex_power_owned = true;
+
+	pci_lock_rescan_remove();
+	while ((pci_bus = pci_find_next_bus(pci_bus)) != NULL) {
+		result = pci_rescan_bus(pci_bus);
+		printk(KERN_INFO "apex_power_open: found %d child-busses.\n", result);
+	}
+	pci_unlock_rescan_remove();
+
+	apex_dev = pci_get_device(APEX_PCI_VENDOR_ID, APEX_PCI_DEVICE_ID, NULL);
+	if (!apex_dev) {
+		printk(KERN_ERR "apex_power: can't find Apex on PCI bus?!\n");
+		return -EIO;
+	}
+	pci_dev_put(apex_dev);
+
+	return 0;
+}
+
+static const struct file_operations apex_power_fops = {
+	.owner = THIS_MODULE,
+	.open = apex_power_open,
+	.release = apex_power_release,
+};
+
+static int __init apex_power_init(void)
+{
+	dev_t apex_power_dev;
+	int apex_power_major;
+	int ret = 0;
+
+	apex_power_class = class_create(THIS_MODULE, "apex_power");
+	if (IS_ERR(apex_power_class)) {
+		printk(KERN_ERR "apex_power: could not allocate device class\n");
+		return -ENODEV;
+	}
+
+	ret = alloc_chrdev_region(&apex_power_dev, 0, 1, "apex_power");
+	apex_power_major = MAJOR(apex_power_dev);
+	if (ret < 0) {
+		printk(KERN_ERR "apex_power: alloc_chrdev_region() failed\n");
+		class_destroy(apex_power_class);
+		return -ENODEV;
+	}
+
+	apex_power_dev = MKDEV(apex_power_major, 0);
+	cdev_init(&apex_power_cdev, &apex_power_fops);
+	ret = cdev_add(&apex_power_cdev, apex_power_dev, 1);
+
+	if (ret) {
+		printk(KERN_ERR "apex_power: Unable to add cdev!\n");
+		unregister_chrdev_region(apex_power_dev, 1);
+		class_destroy(apex_power_class);
+	}
+
+	apex_power_device = device_create(apex_power_class, NULL, apex_power_dev,
+					  NULL, "%s", "apex_power");
+	if (IS_ERR(apex_power_device)) {
+		printk(KERN_ERR "apex_power: device_Create failed.\n");
+		cdev_del(&apex_power_cdev);
+		unregister_chrdev_region(apex_power_dev, 1);
+		class_destroy(apex_power_class);
+	}
+
+	printk(KERN_INFO "apex_power: initialized.\n");
+
+	return 0;
+}
+
+static void __exit apex_power_exit(void)
+{
+	device_del(apex_power_device);
+	cdev_del(&apex_power_cdev);
+	unregister_chrdev_region(MKDEV(apex_power_major, 0), 1);
+	class_destroy(apex_power_class);
+}
+
+module_init(apex_power_init);
+module_exit(apex_power_exit);
+MODULE_DESCRIPTION("Google Apex power management driver");
+MODULE_AUTHOR("June Tate-Gans <jtgans@google.com>");
+MODULE_ALIAS("platform:apex-power");
+MODULE_LICENSE("GPL");
