92f3a5bf79bdbd46d6624cfdcb1dbcceb2392078
Michael Hoang <hoangm@google.com>
2018-12-14 10:00:25 -0800

    Expose Type-C OTG port to sysfs
    - Add ability to dynamically change between host and peripheral modes
    - Since port does not support PDO, do not add ability to select
    [source|sink] for power_role
    
    Tested:
    - "echo [host|device] > /sys/class/typec/port0/data_role"
    
    Change-Id: I14e6b8b1e893aaae417c3c9c908e368f8e76f962

--- drivers/extcon/extcon-ptn5150.c
+++ drivers/extcon/extcon-ptn5150.c
@@ -18,13 +18,14 @@
  */
 
 #include <linux/err.h>
+#include <linux/extcon.h>
+#include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/regmap.h>
-#include <linux/extcon.h>
-#include <linux/gpio/consumer.h>
+#include <linux/usb/typec.h>
 
 /* PTN_REG_CONTROL */
 #define CONTROL_PORT_STATE_MASK (3 << 1)
@@ -33,14 +34,14 @@
 #define CONTROL_PORT_STATE_DRP (2 << 1)
 #define CONTROL_PORT_INT_MASK (1 << 0)
 /* PTN5150_REG_INT_STATUS */
-#define CABLE_ATTACHED		(1 << 0)
-#define CABLE_DETACHED		(1 << 1)
+#define CABLE_ATTACHED (1 << 0)
+#define CABLE_DETACHED (1 << 1)
 /* PTN5150_REG_CC_STATUS */
-#define IS_DFP_ATTATCHED(val)	(((val) & 0x1c) == 0x4)
-#define IS_UFP_ATTATCHED(val)	(((val) & 0x1c) == 0x8)
-#define IS_NOT_CONNECTED(val)	(((val) & 0x1c) == 0x0)
+#define IS_DFP_ATTATCHED(val) (((val)&0x1c) == 0x4)
+#define IS_UFP_ATTATCHED(val) (((val)&0x1c) == 0x8)
+#define IS_NOT_CONNECTED(val) (((val)&0x1c) == 0x0)
 /* PTN5150_REG_CON_DET */
-#define DISABLE_CON_DET		(1 << 0)
+#define DISABLE_CON_DET (1 << 0)
 /* PTN5150_REG_INT_MASK */
 #define MASK_COMP_CHANGE (1 << 4)
 #define MASK_ROLE_CHANGE (1 << 3)
@@ -48,11 +49,11 @@
 #define MASK_DEBUG_FOUND (1 << 1)
 #define MASK_AUDIO_FOUND (1 << 0)
 
-
 struct ptn5150_info {
 	struct device *dev;
 	struct extcon_dev *edev;
-
+	struct typec_capability typec_caps;
+	struct typec_port *typec_port;
 	struct work_struct wq_detect_cable;
 	struct regmap *regmap;
 };
@@ -90,13 +91,14 @@ enum ptn5150_reg {
 };
 
 static const struct regmap_config ptn5150_regmap_config = {
-	.reg_bits	= 8,
-	.val_bits	= 8,
-	.max_register	= PTN5150_REG_END,
+	.reg_bits = 8,
+	.val_bits = 8,
+	.max_register = PTN5150_REG_END,
 	.cache_type = REGCACHE_NONE,
 };
 
-static irqreturn_t ptn5150_i2c_irq_handler(int irq, void *dev_id) {
+static irqreturn_t ptn5150_i2c_irq_handler(int irq, void *dev_id)
+{
 	int ret;
 	unsigned int val = 0;
 	struct ptn5150_info *info = dev_id;
@@ -108,14 +110,13 @@ static irqreturn_t ptn5150_i2c_irq_handler(int irq, void *dev_id) {
 	}
 
 	queue_work(system_power_efficient_wq, &info->wq_detect_cable);
-
 	return IRQ_HANDLED;
 }
 
 static void ptn5150_detect_cable(struct work_struct *work)
 {
-	struct ptn5150_info *info = container_of(work, struct ptn5150_info,
-			wq_detect_cable);
+	struct ptn5150_info *info =
+		container_of(work, struct ptn5150_info, wq_detect_cable);
 	int ret;
 	unsigned int val;
 
@@ -126,17 +127,15 @@ static void ptn5150_detect_cable(struct work_struct *work)
 
 	if (IS_UFP_ATTATCHED(val)) {
 		extcon_set_state_sync(info->edev, EXTCON_USB, false);
-		extcon_set_state_sync(info->edev, EXTCON_USB_HOST,
-			true);
+		extcon_set_state_sync(info->edev, EXTCON_USB_HOST, true);
 	} else if (IS_DFP_ATTATCHED(val)) {
-		extcon_set_state_sync(info->edev, EXTCON_USB_HOST,
-			false);
+		extcon_set_state_sync(info->edev, EXTCON_USB_HOST, false);
 		extcon_set_state_sync(info->edev, EXTCON_USB, true);
 	} else if (IS_NOT_CONNECTED(val)) {
 		extcon_set_state_sync(info->edev, EXTCON_USB, false);
 		extcon_set_state_sync(info->edev, EXTCON_USB_HOST, false);
 	} else {
-		dev_dbg(info->dev, "other CC status is :0x%x", val);
+		dev_dbg(info->dev, "other CC status is :0x%x\n", val);
 	}
 }
 
@@ -147,8 +146,7 @@ static int ptn5150_clear_interrupt(struct ptn5150_info *info)
 
 	ret = regmap_read(info->regmap, PTN5150_REG_INT_STATUS, &val);
 	if (ret) {
-		dev_err(info->dev,
-			"failed to clear interrupt status:%d\n",
+		dev_err(info->dev, "failed to clear interrupt status:%d\n",
 			ret);
 	}
 
@@ -166,7 +164,9 @@ static irqreturn_t ptn5150_connect_irq_handler(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-static int ptn5150_setup_con_det(struct ptn5150_info *info, struct gpio_desc *connect_gpiod) {
+static int ptn5150_setup_con_det(
+	struct ptn5150_info *info, struct gpio_desc *connect_gpiod)
+{
 	int ret, connect_irq, gpio_val, count = 1000;
 
 	connect_irq = gpiod_to_irq(connect_gpiod);
@@ -194,18 +194,17 @@ static int ptn5150_setup_con_det(struct ptn5150_info *info, struct gpio_desc *co
 	}
 
 	ret = regmap_update_bits(info->regmap, PTN5150_REG_CON_DET,
-			DISABLE_CON_DET, ~DISABLE_CON_DET);
+		DISABLE_CON_DET, ~DISABLE_CON_DET);
 	if (ret) {
 		dev_err(info->dev,
-			"failed to enable CON_DET output on pin 5:%d\n",
+			"failed to configure CON_DET output on pin 5:%d\n",
 			ret);
 		return ret;
 	}
 
 	ret = devm_request_threaded_irq(info->dev, connect_irq, NULL,
-					ptn5150_connect_irq_handler,
-					IRQF_TRIGGER_LOW | IRQF_ONESHOT,
-					dev_name(info->dev), info);
+		ptn5150_connect_irq_handler, IRQF_TRIGGER_LOW | IRQF_ONESHOT,
+		dev_name(info->dev), info);
 	if (ret < 0) {
 		dev_err(info->dev, "failed to request connect IRQ\n");
 		return ret;
@@ -214,16 +213,17 @@ static int ptn5150_setup_con_det(struct ptn5150_info *info, struct gpio_desc *co
 	return 0;
 }
 
-static int ptn5150_setup_i2c_det(struct ptn5150_info *info, struct i2c_client *i2c) {
+static int ptn5150_setup_i2c_det(
+	struct ptn5150_info *info, struct i2c_client *i2c)
+{
 	int ret;
 
 	ret = regmap_update_bits(info->regmap, PTN5150_REG_INT_MASK,
 		(MASK_ROLE_CHANGE | MASK_COMP_CHANGE | MASK_ORIENT_FOUND),
-		~(MASK_ROLE_CHANGE | MASK_COMP_CHANGE | MASK_ORIENT_FOUND)
-	);
+		~(MASK_ROLE_CHANGE | MASK_COMP_CHANGE | MASK_ORIENT_FOUND));
 	if (ret) {
-		dev_err(info->dev,
-		"Failed to set I2C interrupt masks: %d", ret);
+		dev_err(info->dev, "Failed to set I2C interrupt masks: %d",
+			ret);
 		return ret;
 	}
 
@@ -231,8 +231,7 @@ static int ptn5150_setup_i2c_det(struct ptn5150_info *info, struct i2c_client *i
 		CONTROL_PORT_STATE_MASK | CONTROL_PORT_INT_MASK,
 		CONTROL_PORT_STATE_UFP | CONTROL_PORT_INT_MASK);
 	if (ret) {
-		dev_err(info->dev,
-		"Failed to set port mode: %d", ret);
+		dev_err(info->dev, "Failed to set port mode: %d", ret);
 		return ret;
 	}
 
@@ -247,15 +246,73 @@ static int ptn5150_setup_i2c_det(struct ptn5150_info *info, struct i2c_client *i
 	return 0;
 }
 
-static int ptn5150_i2c_probe(struct i2c_client *i2c,
-				 const struct i2c_device_id *id)
+static int ptn5150_set_dr(
+	const struct typec_capability *caps, enum typec_data_role role)
+{
+	struct ptn5150_info *info =
+		container_of(caps, struct ptn5150_info, typec_caps);
+	int ret;
+
+	if (role == TYPEC_DEVICE) {
+		ret = regmap_update_bits(info->regmap, PTN5150_REG_CONTROL,
+			CONTROL_PORT_STATE_MASK | CONTROL_PORT_INT_MASK,
+			CONTROL_PORT_STATE_UFP | CONTROL_PORT_INT_MASK);
+
+		if (ret < 0) {
+			dev_err(info->dev,
+				"Failed to change data role to device: %d\n",
+				ret);
+			return ret;
+		}
+		typec_set_data_role(info->typec_port, TYPEC_DEVICE);
+		dev_dbg(info->dev, "Setting Type-C port to peripheral mode.\n");
+	}
+
+	else if (role == TYPEC_HOST) {
+		ret = regmap_update_bits(info->regmap, PTN5150_REG_CONTROL,
+			CONTROL_PORT_STATE_MASK | CONTROL_PORT_INT_MASK,
+			CONTROL_PORT_STATE_DFP | CONTROL_PORT_INT_MASK);
+
+		if (ret < 0) {
+			dev_err(info->dev,
+				"Failed to change data role to host: %d\n",
+				ret);
+			return ret;
+		}
+		typec_set_data_role(info->typec_port, TYPEC_HOST);
+		dev_dbg(info->dev, "Setting Type-C port to host mode.\n");
+	}
+
+	else {
+		dev_err(info->dev, "Unable to change Type-C data role\n.");
+		return -1;
+	}
+	return 0;
+}
+
+struct typec_port *ptn5150_register_port(struct ptn5150_info *info)
+{
+	info->typec_caps.type = TYPEC_PORT_DRP;
+	info->typec_caps.revision = 0x0110;
+	info->typec_caps.prefer_role = TYPEC_SINK;
+	info->typec_caps.dr_set = ptn5150_set_dr;
+
+	info->typec_port = typec_register_port(info->dev, &info->typec_caps);
+	if (!info->typec_port) {
+		dev_err(info->dev, "Unable to register Type-C port.\n");
+	}
+
+	return info->typec_port;
+}
+
+static int ptn5150_i2c_probe(
+	struct i2c_client *i2c, const struct i2c_device_id *id)
 {
 	struct device_node *np = i2c->dev.of_node;
 	struct ptn5150_info *info;
 	int ret;
 	unsigned int dev_id;
 	struct gpio_desc *connect_gpiod;
-
 	if (!np) {
 		return -EINVAL;
 	}
@@ -271,7 +328,7 @@ static int ptn5150_i2c_probe(struct i2c_client *i2c,
 	if (IS_ERR(info->regmap)) {
 		ret = PTR_ERR(info->regmap);
 		dev_err(info->dev, "failed to allocate register map: %d\n",
-				   ret);
+			ret);
 		return ret;
 	}
 
@@ -298,8 +355,9 @@ static int ptn5150_i2c_probe(struct i2c_client *i2c,
 	if (!IS_ERR(connect_gpiod)) {
 		ret = ptn5150_setup_con_det(info, connect_gpiod);
 		if (ret) {
-		  dev_err(info->dev, "Failed to setup connection detection feature\n");
-		  return ret;
+			dev_err(info->dev,
+				"Failed to setup connection detection feature\n");
+			return ret;
 		}
 	}
 
@@ -310,19 +368,21 @@ static int ptn5150_i2c_probe(struct i2c_client *i2c,
 	}
 
 	dev_dbg(info->dev, "NXP PTN5150: Version ID:0x%x, Vendor ID:0x%x\n",
-			(dev_id >> 3), (dev_id & 0x3));
+		(dev_id >> 3), (dev_id & 0x3));
 
 	if (i2c->irq) {
 		ret = ptn5150_setup_i2c_det(info, i2c);
 		if (ret) {
-			dev_err(info->dev, "Failed to configure i2c interrupts: %d", ret);
+			dev_err(info->dev,
+				"Failed to configure i2c interrupts: %d\n",
+				ret);
 			return ret;
 		}
 	}
 
 	ret = ptn5150_clear_interrupt(info);
 	if (ret < 0) {
-		dev_err(info->dev, "Failed to clear interrupt: %d", ret);
+		dev_err(info->dev, "Failed to clear interrupt: %d\n", ret);
 		return ret;
 	}
 
@@ -330,6 +390,9 @@ static int ptn5150_i2c_probe(struct i2c_client *i2c,
 
 	/* Do cable detect now */
 	ptn5150_detect_cable(&info->wq_detect_cable);
+
+	/* Register port to sysfs */
+	info->typec_port = ptn5150_register_port(info);
 
 	return ret;
 }
@@ -341,27 +404,32 @@ static int ptn5150_i2c_remove(struct i2c_client *i2c)
 
 static const struct i2c_device_id ptn5150_id[] = {
 	{ "ptn5150", 0 },
-	{ },
+	{},
 };
 MODULE_DEVICE_TABLE(i2c, ptn5150_id);
 
 #ifdef CONFIG_OF
 static const struct of_device_id ptn5150_of_match[] = {
-	{ .compatible = "nxp,ptn5150", },
-	{ .compatible = "nxp,ptn5150a", },
+	{
+		.compatible = "nxp,ptn5150",
+	},
+	{
+		.compatible = "nxp,ptn5150a",
+	},
 	{},
 };
 MODULE_DEVICE_TABLE(of, ptn5150_of_match);
 #endif
 
 static struct i2c_driver ptn5150_i2c_driver = {
-	.driver = {
-		.name = "ptn5150",
-		.of_match_table = of_match_ptr(ptn5150_of_match),
-	},
-	.probe		= ptn5150_i2c_probe,
-	.remove		= ptn5150_i2c_remove,
-	.id_table	= ptn5150_id,
+    .driver =
+        {
+            .name = "ptn5150",
+            .of_match_table = of_match_ptr(ptn5150_of_match),
+        },
+    .probe = ptn5150_i2c_probe,
+    .remove = ptn5150_i2c_remove,
+    .id_table = ptn5150_id,
 };
 module_i2c_driver(ptn5150_i2c_driver);
 
