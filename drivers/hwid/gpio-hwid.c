ac1d961752721362ed37da66d9d6e876ea6cc988
frank_chiang <Frank_Chiang@asus.com>
2019-08-21 10:33:20 +0800

    hwid: Set ID pin muxes to gpio.
    Correct the value get from gpio in hwid driver.
    
    Change-Id: I9e1cdae0fb4d70b3d96dfb7e20b1cb555a6652a5

--- drivers/hwid/gpio-hwid.c
+++ drivers/hwid/gpio-hwid.c
@@ -4,7 +4,7 @@
 #include <linux/of_platform.h>
 #include <linux/of_gpio.h>
 
-static int hwid;
+static int id0_gpio, id1_gpio, id2_gpio;
 
 static const struct of_device_id of_gpio_hwid_match[] = {
 	{ .compatible = "gpio-hwid", },
@@ -12,17 +12,19 @@ static const struct of_device_id of_gpio_hwid_match[] = {
 };
 MODULE_DEVICE_TABLE(of, of_gpio_hwid_match);
 
-static int set_hwid(int id2_pin, int id1_pin, int id0_pin)
-{
-	int id;
-	id = (id2_pin << 2) + (id1_pin << 1) + id0_pin;
-	return id;
-}
-
 static ssize_t hwid_show(struct device *dev,
 		struct device_attribute *attr,
 		char *buf)
 {
+	int id0, id1, id2;
+	int hwid;
+
+	id0 = gpio_get_value(id0_gpio);
+	id1 = gpio_get_value(id1_gpio);
+	id2 = gpio_get_value(id2_gpio);
+
+	hwid = (id2 << 2) + (id1 << 1) + id0;
+
 	return sprintf(buf, "%d\n", hwid);
 }
 
@@ -31,57 +33,68 @@ static DEVICE_ATTR(HWID, S_IRUGO, hwid_show, NULL);
 static int gpio_hwid_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	int id0_gpio, id1_gpio, id2_gpio;
-	int id0, id1, id2;
 	int ret;
 
 	id0_gpio = of_get_named_gpio(dev->of_node, "id0-gpios", 0);
 	if (!gpio_is_valid(id0_gpio)) {
 		printk("No id0-gpio pin available in gpio-hwid\n");
-		return 1;
+		return -ENODEV;
 	} else {
-		id0 = devm_gpio_request_one(dev, id0_gpio, GPIOF_DIR_IN, "GPIO_ID0");
-		if (id0 < 0)
-			printk("Fail to set id0 pin\n");
+		ret = devm_gpio_request_one(dev, id0_gpio, GPIOF_DIR_IN, "GPIO_ID0");
+		if (ret < 0) {
+			printk("Failed to request ID0 gpio: %d\n", ret);
+			return ret;
+		}
 	}
 
 	id1_gpio = of_get_named_gpio(dev->of_node, "id1-gpios", 0);
 	if (!gpio_is_valid(id1_gpio)) {
 		printk("No id1-gpio pin available in gpio-hwid\n");
-		return 1;
+		return -ENODEV;
 	} else {
-		id1 = devm_gpio_request_one(dev, id1_gpio, GPIOF_DIR_IN, "GPIO_ID1");
-		if (id1 < 0)
-			printk("Fail to set id1 pin\n");
+		ret = devm_gpio_request_one(dev, id1_gpio, GPIOF_DIR_IN, "GPIO_ID1");
+		if (ret < 0) {
+			printk("Failed to request ID1 gpio: %d\n", ret);
+			return ret;
+		}
 	}
 
 	id2_gpio = of_get_named_gpio(dev->of_node, "id2-gpios", 0);
 	if (!gpio_is_valid(id2_gpio)) {
 		printk("No id2-gpio pin available in gpio-hwid\n");
-		return 1;
+		return -ENODEV;
 	} else {
-		id2 = devm_gpio_request_one(dev, id2_gpio, GPIOF_DIR_IN, "GPIO_ID2");
-		if (id2 < 0)
-			printk("Fail to set id2 pin\n");
+		ret = devm_gpio_request_one(dev, id2_gpio, GPIOF_DIR_IN, "GPIO_ID2");
+		if (ret < 0) {
+			printk("Failed to request ID2 gpio: %d\n", ret);
+			return ret;
+		}
 	}
-
-	hwid = set_hwid(id2, id1, id0);
-	printk("HWID = %d\n", hwid);
 
 	ret = device_create_file(dev, &dev_attr_HWID);
 	if (ret) {
 		printk("Cannot create HWID attribute\n");
 		device_remove_file(dev, &dev_attr_HWID);
-		return 1;
+		return -ENODEV;
 	}
 
 	return 0;
 }
 
+static int gpio_hwid_remove(struct platform_device *pdev)
+{
+	gpio_free(id0_gpio);
+	gpio_free(id1_gpio);
+	gpio_free(id2_gpio);
+
+	return 0;
+}
+
 static struct platform_driver gpio_hwid_driver = {
-	.probe          = gpio_hwid_probe,
+	.probe		= gpio_hwid_probe,
+	.remove		= gpio_hwid_remove,
 	.driver = {
-		.name   = "gpio-hwid",
+		.name	= "gpio-hwid",
 #ifdef CONFIG_OF_GPIO
 		.of_match_table = of_match_ptr(of_gpio_hwid_match),
 #endif
